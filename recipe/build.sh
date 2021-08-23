c9a47b3d0e5d4481394aab1b19d7d569caa30f68
Kai Tietz <ktietz@anaconda.com>
2021-08-23 12:13:14 +0200

    add osx-arm64 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,14 +8,26 @@
 CFLAGS="${CFLAGS} -O3"
 CXXFLAGS="${CXXFLAGS} -O3"
 
+if [[ "$target_platform" == "osx-64" ]]; then
+    CXXFLAGS="$CXXFLAGS -DTARGET_OS_OSX=1"
+    CFLAGS="$CFLAGS -DTARGET_OS_OSX=1"
+fi
+
+if [[ "$target_platform" == "osx-arm64" ]]; then
+    CMAKE_ARGS="${CMAKE_ARGS} -DPCRE2_SUPPORT_JIT=OFF"
+else
+    CMAKE_ARGS="${CMAKE_ARGS} -DPCRE2_SUPPORT_JIT=ON"
+fi
+
 mkdir build_cmake
 pushd build_cmake
-cmake \
+cmake ${CMAKE_ARGS} \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_BUILD_TYPE=release \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DPCRE2_SUPPORT_JIT=ON \
+    -DPCRE2_BUILD_PCRE2_16=ON \
+    -DPCRE2_BUILD_PCRE2_32=ON \
     -GNinja \
     ..
 
